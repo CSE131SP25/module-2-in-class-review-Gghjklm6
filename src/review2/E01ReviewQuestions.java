9b0edaa49bc4fbb65a06f850efba8737931114ae
gghjk <gghjk@DESKTOP-8646QEE.dhcp.wustl.edu>
2025-01-27 14:17:11 -0600

    j

--- src/review2/E01ReviewQuestions.java
+++ src/review2/E01ReviewQuestions.java
@@ -6,13 +6,45 @@ public class E01ReviewQuestions {
 		
 		//1. What is a relational operator?
 		
+		//Comparison operator (<,>, =)
+		
 		//2. What is a logical operator?
+		
+		//&& (and), || (or), ! (not)
 		
 		//3. What does an if statement need to do its job?
 		
+		//used to make choices and decisions
+		
+		int x = 2;
+		int y = 6;
+		if(x<y) {
+			//put any codes we want
+			//will run this if condition is true
+		}
+		
 		//4. What is the difference between else and else if?
 		
+		//introduces new conditions to consider
+		
+		if (x<y) {
+			//do something
+		} else if (x < 9) {
+			//do something else
+		} else {
+			//do this if all are false
+		}
+		
 		//5. What are the different parts of a loop?
+		
+		for(int i = 0; i <10; i ++) {
+			System.out.println();
+		}
+		
+		int i = 0;
+		while (i<10) {
+			i++;
+		}
 		
 		//6. What's the difference between a for loop and a while loop?
 			//6a. When should you choose one over the other?
--- src/review2/E03Fibonacci.java
+++ src/review2/E03Fibonacci.java
@@ -12,5 +12,21 @@ public class E03Fibonacci {
 	 */
 	public static void main(String[] args) {
 		
+		
+		System.out.println(0);
+		System.out.println(1);
+		
+		long i = 0;
+		long previous1 = 0;
+		long previous2 = 1;
+		while (i<50) {
+			i++;
+			long newnum = previous1 + previous2;
+			System.out.println(newnum);
+			System.out.println("-------------------------");
+			previous1 = previous2;
+			previous2 = newnum;
+		}
+		
 	}
 }
